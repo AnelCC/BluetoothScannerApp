e74308ccef68a0e7dc3fc22cd3f349ad7e262d21
AnelCC <anel.calvo.cervantes@gmail.com>
2023-10-25 21:36:46 -0400

    Add lazy column

--- app/src/main/java/com/anelcc/bluetoothscanner/presentation/ui/ListScreen.kt
+++ app/src/main/java/com/anelcc/bluetoothscanner/presentation/ui/ListScreen.kt
@@ -1,8 +1,18 @@
 package com.anelcc.bluetoothscanner.presentation.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ListScreen () {
-
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(5) { id ->
+            Text("This is item #${id+1}")
+        }
+    }
 }
