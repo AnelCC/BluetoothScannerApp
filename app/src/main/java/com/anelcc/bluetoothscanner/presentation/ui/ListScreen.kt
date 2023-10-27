6bea5a61b0aac300c77d5c058a439e01667720a9
AnelCC <anel.calvo.cervantes@gmail.com>
2023-10-26 22:08:40 -0400

    Add Scaffold and TopBar

--- app/src/main/java/com/anelcc/bluetoothscanner/presentation/ui/ListScreen.kt
+++ app/src/main/java/com/anelcc/bluetoothscanner/presentation/ui/ListScreen.kt
@@ -1,18 +1,58 @@
 package com.anelcc.bluetoothscanner.presentation.ui
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.anelcc.bluetoothscanner.R
+import com.anelcc.bluetoothscanner.presentation.theme.BluetoothScannerTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListScreen () {
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        items(5) { id ->
-            Text("This is item #${id+1}")
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(stringResource(R.string.app_name))
+                }
+            )
+        },
+    ) { innerPadding ->
+
+        LazyColumn(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(5) { id ->
+                Text("This is item #${id+1}")
+            }
         }
+    }
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BluetoothScannerTheme {
+        ListScreen()
     }
 }
