57121fe211c9a4eab2464f4014cda3b395e9211d
AnelCC <anel.calvo.cervantes@gmail.com>
2023-11-29 19:43:43 -0500

    Add view model, basic function and link with the view

--- app/src/main/java/com/anelcc/bluetoothscanner/presentation/MainActivity.kt
+++ app/src/main/java/com/anelcc/bluetoothscanner/presentation/MainActivity.kt
@@ -4,18 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.anelcc.bluetoothscanner.domain.ScanState
+import androidx.activity.viewModels
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.anelcc.bluetoothscanner.presentation.theme.BluetoothScannerTheme
 import com.anelcc.bluetoothscanner.presentation.ui.BluetoothScannerScreen
+import com.anelcc.bluetoothscanner.presentation.ui.BluetoothScannerViewModel
 
-class MainActivity : ComponentActivity() {
+class MainActivity() : ComponentActivity() {
+    private val viewModel: BluetoothScannerViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             BluetoothScannerTheme {
+                val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
                 BluetoothScannerScreen(
-                    state = ScanState(),
+                    state = uiState,
                     onStartScan = { /* Handle start scan */ },
                     onStopScan = { /* Handle stop scan */ },
                     onClearError = { /* Handle clear error */ }
--- app/src/main/java/com/anelcc/bluetoothscanner/presentation/ui/BluetoothScannerViewModel.kt
+++ app/src/main/java/com/anelcc/bluetoothscanner/presentation/ui/BluetoothScannerViewModel.kt
@@ -1,9 +1,35 @@
 package com.anelcc.bluetoothscanner.presentation.ui
 
 import androidx.lifecycle.ViewModel
+import com.anelcc.bluetoothscanner.domain.ScanState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 
 class BluetoothScannerViewModel() : ViewModel() {
 
+    private val _uiState = MutableStateFlow(ScanState())
+    val uiState: StateFlow<ScanState> = _uiState.asStateFlow()
+
+
+    init {
+       // fetch Bluetooth data
+    }
+
+    private fun observeBluetoothData() {
+        // Observe Bluetooth data and update the UI state
+    }
+
+    fun startScan() {
+        // Handle start scan
+    }
+    fun stopScan() {
+        // Handle stop scan
+    }
+
+    fun clearError() {
+        _uiState.value = _uiState.value.copy(error = null)
+    }
 }
 
