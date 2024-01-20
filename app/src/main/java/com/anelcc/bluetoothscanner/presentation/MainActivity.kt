eb5a88e4f6bf3970ff8a7508869d087038c91777
AnelCC <anel.calvo.cervantes@gmail.com>
2024-01-20 09:16:54 -0400

    Update Ui to prepare for add the viewmodel

--- app/src/main/java/com/anelcc/bluetoothscanner/presentation/MainActivity.kt
+++ app/src/main/java/com/anelcc/bluetoothscanner/presentation/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.anelcc.bluetoothscanner.presentation.theme.BluetoothScannerTheme
@@ -12,22 +12,32 @@ import com.anelcc.bluetoothscanner.presentation.ui.BluetoothScannerScreen
 import com.anelcc.bluetoothscanner.presentation.ui.BluetoothScannerViewModel
 
 class MainActivity() : ComponentActivity() {
-    private val viewModel: BluetoothScannerViewModel by viewModels()
+    private lateinit var viewModel: BluetoothScannerViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        // Here Initialize the ViewModel with dependencies
+
+
         setContent {
             BluetoothScannerTheme {
-                val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-
-                BluetoothScannerScreen(
-                    state = uiState,
-                    onStartScan = viewModel::startScan,
-                    onStopScan = viewModel::stopScan,
-                    onClearError = viewModel::clearError
-                )
+                // Here pass the initialized ViewModel to the Composable
+                BluetoothScannerApp(viewModel)
             }
         }
     }
+}
+
+
+@Composable
+fun BluetoothScannerApp(viewModel: BluetoothScannerViewModel) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    BluetoothScannerScreen(
+        state = uiState,
+        onStartScan = viewModel::startScan,
+        onStopScan = viewModel::stopScan,
+        onClearError = viewModel::clearError
+    )
 }
