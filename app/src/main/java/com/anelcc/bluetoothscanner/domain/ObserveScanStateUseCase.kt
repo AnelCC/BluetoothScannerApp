5923b688e50aee48ebb11ea4f0cbffd9c4badcf3
AnelCC <anel.calvo.cervantes@gmail.com>
2024-03-22 23:02:53 -0500

    request Permission in the activity

--- app/src/main/java/com/anelcc/bluetoothscanner/domain/ObserveScanStateUseCase.kt
+++ app/src/main/java/com/anelcc/bluetoothscanner/domain/ObserveScanStateUseCase.kt
@@ -2,7 +2,6 @@ package com.anelcc.bluetoothscanner.domain
 
 import com.anelcc.bluetoothscanner.core.BluetoothRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
 
 class ObserveScanStateUseCase(private val repository: BluetoothRepository) {
     suspend operator fun invoke(): Flow<Boolean> {
--- app/src/main/java/com/anelcc/bluetoothscanner/presentation/MainActivity.kt
+++ app/src/main/java/com/anelcc/bluetoothscanner/presentation/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.anelcc.bluetoothscanner.presentation
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -13,6 +15,15 @@ import com.anelcc.bluetoothscanner.presentation.ui.BluetoothScannerViewModel
 
 class MainActivity() : ComponentActivity() {
     private lateinit var viewModel: BluetoothScannerViewModel
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        val allGranted = permissions.values.all { it }
+        if (!allGranted) {
+            Toast.makeText(this, "Bluetooth permissions are required", Toast.LENGTH_LONG).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
