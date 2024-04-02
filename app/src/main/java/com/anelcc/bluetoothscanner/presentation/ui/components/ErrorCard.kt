f7c7b5c8ba8d3780da7bb5f3cde68592bf5d9dc0
AnelCC <anel.calvo.cervantes@gmail.com>
2024-04-01 20:19:34 -0500

    Add Error Card opt the UI

---
+++ app/src/main/java/com/anelcc/bluetoothscanner/presentation/ui/components/ErrorCard.kt
@@ -0,0 +1,8 @@
+package com.anelcc.bluetoothscanner.presentation.ui.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ErrorCard(error: String, onDismiss: () -> Unit) {
+
+}
