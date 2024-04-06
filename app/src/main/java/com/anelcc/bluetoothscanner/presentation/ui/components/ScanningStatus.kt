5a1dbeaeb2d97ba8059d67381c7b316e08c1def9
AnelCC <anel.calvo.cervantes@gmail.com>
2024-04-06 10:32:11 -0500

    Create Scanning status to the UI

--- app/src/main/java/com/anelcc/bluetoothscanner/presentation/ui/components/ScanningStatus.kt
+++ app/src/main/java/com/anelcc/bluetoothscanner/presentation/ui/components/ScanningStatus.kt
@@ -1,4 +1,14 @@
 package com.anelcc.bluetoothscanner.presentation.ui.components
 
-class ScanningStatus {
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ScanningStatus (isScanning: Boolean, deviceCount: Int) {
+    Text(
+        text = if (isScanning) "Scanning for devices..." else "Found $deviceCount devices",
+        style = MaterialTheme.typography.bodyLarge,
+        color = MaterialTheme.colorScheme.onSurfaceVariant
+    )
 }
